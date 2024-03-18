46d28be00a528ca2d6a02db3942c6ea46ea9395a
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2024-03-18 06:26:09 -0300

    Add copilot chat and tmux navigator

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    opts = {},
+    cmd = {
+      "CopilotChat",
+      "CopilotChatOpen",
+      "CopilotChatClose",
+      "CopilotChatToggle",
+      "CopilotChatReset",
+      "CopilotChatDebugInfo",
+      "CopilotChatExplain",
+      "CopilotChatTests",
+      "CopilotChatFix",
+      "CopilotChatOptimize",
+      "CopilotChatDocs",
+      "CopilotChatFixDiagnostic",
+      "CopilotChatCommit",
+      "CopilotChatCommitStaged",
+    },
+  },
+}

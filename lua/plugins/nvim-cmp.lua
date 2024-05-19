3e37142d1d8d54f7a05d54483b3be2e242c5eeca
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2024-05-18 21:38:40 -0300

    Fix completion bindings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,12 +1,33 @@
 local cmp = require("cmp")
 
 return {
-  "hrsh7th/nvim-cmp",
-  opts = {
-    completion = { completeopt = vim.o.completeopt },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
+  -- Enable borders
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {
+      completion = { completeopt = vim.o.completeopt },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
     },
+  },
+  -- Edit some mappings
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
+        ["<C-c>"] = cmp.mapping.abort(),
+        ["<CR>"] = function(fallback)
+          if cmp.visible() then
+            LazyVim.create_undo()
+            if cmp.confirm({ select = false }) then
+              return
+            end
+          end
+          return fallback()
+        end,
+      })
+    end,
   },
 }

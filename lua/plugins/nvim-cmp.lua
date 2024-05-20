2d24c31ebb37e4e23f57ee4a9a911c33aca6a63e
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2024-05-20 19:42:11 -0300

    cmp: use lazyvim confirm wrapper

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -18,15 +18,7 @@ return {
     opts = function(_, opts)
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
         ["<C-c>"] = cmp.mapping.abort(),
-        ["<CR>"] = function(fallback)
-          if cmp.visible() then
-            LazyVim.create_undo()
-            if cmp.confirm({ select = false }) then
-              return
-            end
-          end
-          return fallback()
-        end,
+        ["<CR>"] = LazyVim.cmp.confirm({ select = false }),
       })
     end,
   },

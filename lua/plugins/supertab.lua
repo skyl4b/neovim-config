e90125f732db269acd4a8bfec129e75dd8d0372a
Eduardo Farinati <eduardo.farinati@ufrgs.br>
2024-05-20 19:28:49 -0300

    cmp: switch to improved visible version

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -20,7 +20,7 @@ return {
       -- Set mapping
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
+          if cmp.core.view:visible() or vim.fn.pumvisible() == 1 then
             -- You could replace select_next_item() with confirm({ select = true })
             -- to get VS Code autocompletion behavior
             cmp.select_next_item()
@@ -37,7 +37,7 @@ return {
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
+          if cmp.core.view:visible() or vim.fn.pumvisible() == 1 then
             cmp.select_prev_item()
           elseif vim.snippet.active({ direction = -1 }) then
             vim.schedule(function()
